28c8c457cbc1991189c0409ffaee1a5edc57460c
Ridbir <andre.good2016@gmail.com>
2024-12-19 16:21:20 +0500

    Добавил подчеркивания ошибок Prettier при помощи ESLint

--- RadioImpulsFmWeb/eslint.config.mjs
+++ RadioImpulsFmWeb/eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] }, // Разрешить файлы
@@ -11,8 +12,14 @@ export default [
         __dirname: true, // Разрешить __dirname
         require: true, // Разрешить функцию require()
         module: true,
-      }
-    }
+      },
+    },
+    plugins: {
+      prettier: pluginPrettier, // Добавление связи с Prettier
+    },
+    rules: {
+      "prettier/prettier": "error", // Добавление связи с Prettier
+    },
   },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
