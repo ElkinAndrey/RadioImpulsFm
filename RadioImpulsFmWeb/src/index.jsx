d657e41a72fdb5fa8e1ce9c36134a0c81aa0e0d9
ElkinAndrey <andreyridbir@gmail.com>
2024-12-27 14:09:23 +0500

    Добавил базовые стили в проект

--- RadioImpulsFmWeb/src/index.jsx
+++ RadioImpulsFmWeb/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { App } from "./app";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("root not found");
